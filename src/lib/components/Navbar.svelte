26268ad8947a3c87c6c06a9cb1976ac4c92d6aaf
Nuno Casteleira <nunojoaocas@gmail.com>
2022-10-04 23:29:33 +0100

    feat(layout): :sparkles: add active class to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,3 +1,7 @@
+<script lang ts>
+	import { page } from '$app/stores';
+</script>
+
 <header class="col-span-3 bg-daisyBush text-center">
 	<div>
 		<a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
@@ -6,9 +10,9 @@
 	<nav>
 		<ul class="list-none text-2xl font-bold">
 			<li>
-				<a href="/invoices">Invoices</a>
+				<a href="/invoices" class:active={$page.url.pathname === '/invoices'}>Invoices</a>
 			</li>
-			<li><a href="/clients">Clients</a></li>
+			<li><a href="/clients" class:active={$page.url.pathname === '/clients'}>Clients</a></li>
 			<li><a href="#">Settings</a></li>
 			<li><a href="#">Logout</a></li>
 		</ul>
@@ -22,5 +26,15 @@
 
 	nav ul li a {
 		@apply font-bold text-white hover:text-goldenFizz;
+	}
+
+	nav ul li a.active {
+		@apply px-8 text-robinEggBlue transition-[padding];
+		background: url('/images/active-nav--left.svg') left top no-repeat,
+			url('/images/active-nav--right.svg') right top no-repeat;
+	}
+
+	nav ul li a.active:hover {
+		@apply px-9;
 	}
 </style>
