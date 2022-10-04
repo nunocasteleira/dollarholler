0e165b346c3451547931553bae7d0209ff2a9b17
Nuno Casteleira <nunojoaocas@gmail.com>
2022-10-04 23:18:18 +0100

    refactor(layout): :art: abstract navbar to its own component

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,26 @@
+<header class="col-span-3 bg-daisyBush text-center">
+	<div>
+		<a href="/invoices"><img src="/images/logo.svg" alt="The Dollar Holler" class="mx-auto" /></a>
+	</div>
+
+	<nav>
+		<ul class="list-none text-2xl font-bold">
+			<li>
+				<a href="/invoices">Invoices</a>
+			</li>
+			<li><a href="/clients">Clients</a></li>
+			<li><a href="#">Settings</a></li>
+			<li><a href="#">Logout</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="postcss">
+	nav ul li {
+		@apply mb-6;
+	}
+
+	nav ul li a {
+		@apply font-bold text-white hover:text-goldenFizz;
+	}
+</style>
